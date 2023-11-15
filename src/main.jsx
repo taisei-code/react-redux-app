aea7d0b661a2dce722f43a02b12e4253ede6ad31
Taisei <taisei96.web@gmail.com>
2023-11-16 08:55:05 +0900

    Store・Action・Reducer準備

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createStore } from 'redux'
+
+
+// actions -> increment, decrement
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// reducer -> アクションと前の状態を組み合わせて新しい状態に更新する
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+// store
+let store = createStore(counterReducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
