73b60680711db02f524e361f6e73383086f115d4
Taisei <taisei96.web@gmail.com>
2023-11-17 07:35:44 +0900

    複数のReducerを組み合わせる

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createStore } from 'redux'
+import allReducers from './reducers/index.js'
+
+const store = createStore(allReducers);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
