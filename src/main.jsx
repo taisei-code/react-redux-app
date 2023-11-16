4c13ad64b86612012692a802d9a1f6b6685cdde5
Taisei <taisei96.web@gmail.com>
2023-11-16 09:35:33 +0900

    リファクタリング

--- src/main.jsx
+++ src/main.jsx
@@ -4,39 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { createStore } from 'redux'
 
-
-// actions -> increment, decrement
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-// reducer -> アクションと前の状態を組み合わせて新しい状態に更新する
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
-
-// store
-let store = createStore(counterReducer);
-
-// コンソールに新しい状態を出力
-store.subscribe(() => console.log(store.getState()));
-
-// dispatch
-store.dispatch(increment());
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
