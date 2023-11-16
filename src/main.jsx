78a892ece9f26bef433f0052fc8ade2bc67b2fe1
Taisei <taisei96.web@gmail.com>
2023-11-17 08:16:17 +0900

    payload追加

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { createStore } from 'redux'
 import allReducers from './reducers/index.js'
+import { Provider } from "react-redux"
 
-const store = createStore(allReducers);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
